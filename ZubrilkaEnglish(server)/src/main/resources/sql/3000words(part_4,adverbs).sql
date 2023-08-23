0cf92a6f93f5b38de36dfa1de84bec5803bdfd96
KmnPetr <kmn.petrichenko@yandex.ru>
2023-08-23 15:32:39 +0300

    закончил собирать слова, пора их форматировать

---
+++ ZubrilkaEnglish(server)/src/main/resources/sql/3000words(part_4,adverbs).sql
@@ -0,0 +1,200 @@
+--     Время
+now	[naʊ]	сейчас
+then	[ðɛn]	затем
+never	[ˈnɛvə]	никогда
+again	[əˈgɛn]	снова
+always	[ˈɔːlweɪz]	всегда
+today	[təˈdeɪ]	сегодня
+long	[lɒŋ]	долго
+ever	[ˈɛvə]	вечно
+later	[ˈleɪtə]	позже
+(one year) ago	[əˈgəʊ]	(один год) назад
+early	[ˈɜːli]	рано
+soon	[suːn]	скоро
+recently	[ˈriːsntli]	недавно
+before	[bɪˈfɔː]	до
+PM	[piː-ɛm]	после полудня
+tonight	[təˈnaɪt]	этой ночью (вечером)
+AM	[æm]	до полудня
+late	[leɪt]	поздно
+forever	[fəˈrɛvə]	навсегда
+rarely	[ˈreəli]	редко
+tomorrow	[təˈmɒrəʊ]	завтра
+yesterday	[ˈjɛstədeɪ]	вчера
+currently	[ˈkʌrəntli]	в настоящее время
+    already	[ɔːlˈrɛdi]	уже
+previously	[ˈpriːvjəsli]	ранее, в прошлый раз
+whenever	[wɛnˈɛvə]	всякий раз, когда
+just	[ʤʌst]	только что (всего лишь)
+sometimes	[ˈsʌmtaɪmz]	иногда
+finally	[ˈfaɪnəli]	наконец
+quickly	[ˈkwɪkli]	быстро
+suddenly	[ˈsʌdnli]	вдруг
+immediately	[ɪˈmiːdjətli]	немедленно
+frequently	[ˈfriːkwəntli]	часто
+meanwhile	[ˈmiːnˈwaɪl]	тем временем
+occasionally	[əˈkeɪʒnəli]	иногда
+initially	[ɪˈnɪʃəli]	изначально
+shortly	[ˈʃɔːtli]	коротко, вскоре
+gradually	[ˈgrædjʊəli]	постепенно
+ultimately	[ˈʌltɪmɪtli]	в конечном счете
+regularly	[ˈrɛgjʊləli]	регулярно
+eventually	[ɪˈvɛnʧəli]	в конечном итоге
+rapidly	[ˈræpɪdli]	быстро
+newly	[ˈnjuːli]	вновь, только что
+primarily	[ˈpraɪmərɪli]	в первую очередь
+constantly	[ˈkɒnstəntli]	постоянно
+originally	[əˈrɪʤɪnəli]	изначально (по происхождению)
+often	[ˈɒf(ə)n] [ˈɒft(ə)n]	часто
+
+--     Образ действия (наречия)
+    fast	[fɑːst]	быстро
+well	[wɛl]	хорошо
+even	[ˈiːvən]	даже
+too	[tuː]	слишком
+usually	[ˈjuːʒʊəli]	обычно
+exactly	[ɪgˈzæktli]	точно
+ok	[ˈəʊˈkeɪ]	хорошо
+clearly	[ˈklɪəli]	ясно
+directly	[dɪˈrɛktli]	непосредственно
+absolutely	[ˈæbsəluːtli]	абсолютно
+completely	[kəmˈpliːtli]	полностью
+easily	[ˈiːzɪli]	с легкостью
+slowly	[ˈsləʊli]	медленно
+highly	[ˈhaɪli]	высоко
+mostly	[ˈməʊstli]	в основном
+seriously	[ˈsɪərɪəsli]	серьезно
+totally	[ˈtəʊtli]	полностью
+merely	[ˈmɪəli]	просто
+typically	[ˈtɪpɪk(ə)li]	обычно
+simply	[ˈsɪmpli]	просто
+nearly	[ˈnɪəli]	почти
+carefully	[ˈkeəfli]	тщательно
+somehow	[ˈsʌmhaʊ]	как-нибудь
+truly	[ˈtruːli]	действительно
+hardly	[ˈhɑːdli]	с трудом
+significantly	[sɪgˈnɪfɪkəntli]	значительно
+entirely	[ɪnˈtaɪəli]	полностью
+necessarily	[ˈnɛsɪsərɪli]	обязательно
+specifically	[spəˈsɪfɪk(ə)li]	специально
+surely	[ˈʃʊəli]	уверенно, безусловно
+heavily	[ˈhɛvɪli]	сильно
+strongly	[ˈstrɒŋli]	сильно
+quietly	[ˈkwaɪətli]	тихо
+effectively	[ɪˈfɛktɪvli]	эффективно
+precisely	[prɪˈsaɪsli]	точно
+normally	[ˈnɔːməli]	обычно
+roughly	[ˈrʌfli]	примерно (грубо)
+gently	[ˈʤɛntli]	мягко
+naturally	[ˈnæʧrəli]	естественно
+partly	[ˈpɑːtli]	частично
+properly	[ˈprɒpəli]	правильно
+slightly	[ˈslaɪtli]	немного
+straight	[streɪt]	прямо
+definitely	[ˈdɛfɪnɪtli]	определенно
+virtually	[ˈvɜːtjʊəli]	практически
+literally	[ˈlɪtərəli]	буквально
+politically	[pəˈlɪtɪkəli]	политически
+relatively	[ˈrɛlətɪvli]	относительно
+personally	[ˈpɜːsnəli]	лично
+likely	[ˈlaɪkli]	вероятно
+similarly	[ˈsɪmɪləli]	аналогично, похоже
+potentially	[pəʊˈtɛnʃəli]	потенциально
+
+--     Пространство
+    here	[hɪə]	здесь
+around	[əˈraʊnd]	вокруг
+forward	[ˈfɔːwəd]	вперед
+close	[kləʊs]	близко
+forth	[fɔːθ]	вперед
+anywhere	[ˈɛnɪweə]	в любом месте
+deeply	[ˈdiːpli]	глубоко
+ahead (go ahed)	[əˈhɛd]	вперед
+somewhere	[ˈsʌmweə]	где-то
+everywhere	[ˈɛvrɪweə]	везде
+back	[bæk]	назад
+there	[ðeə]	там
+far	[fɑː]	далеко
+away	[əˈweɪ]	вдали от чего-то
+along	[əˈlɒŋ]	вдоль
+below	[bɪˈləʊ]	ниже
+apart	[əˈpɑːt]	отдельно от чего-то
+widely	[ˈwaɪdli]	широко
+outside	[ˌaʊtˈsaɪd]	вне
+aside	[əˈsaɪd]	в сторону
+closely	[ˈkləʊsli]	близко
+nowhere	[ˈnəʊweə]	никуда
+elsewhere	[ˈɛlsˈweə]	в другом месте
+
+--     Количество (наречия)
+    a little	[ə ˈlɪtl]	немного
+only	[ˈəʊnli]	только
+alone	[əˈləʊn]	в одиночку
+at least	[ət liːst]	по меньшей мере
+together	[təˈgɛðə]	вместе
+a lot	[ə lɒt]	много
+bit	[bɪt]	немного
+twice	[twaɪs]	дважды
+fully	[ˈfʊli]	полностью
+increasingly	[ɪnˈkriːsɪŋli]	более
+anymore	[ˌɛniˈmɔː]	(еще) больше
+also	[ˈɔːlsəʊ]	также
+else (something else)	[ɛls]	еще (что-нибудь еще)
+more	[mɔː]	больше
+much	[mʌʧ]	много
+less	[lɛs]	меньше
+all	[ɔːl]	все
+once	[wʌns]	раз (однажды)
+
+--     Мера и степень (наречия)
+    enough	[ɪˈnʌf]	достаточно
+largely	[ˈlɑːʤli]	в значительной степени
+deep	[diːp]	глубоко
+extremely	[ɪksˈtriːmli]	чрезвычайно
+partly	[ˈpɑːtli]	частично
+barely	[ˈbeəli]	едва
+fairly	[ˈfeəli]	довольно
+essentially	[ɪˈsɛnʃəli]	существенно
+approximately	[əˈprɒksɪmɪtli]	примерно
+equally	[ˈiːkwəli]	одинаково
+perfectly	[ˈpɜːfɪktli]	отлично
+pretty	[ˈprɪti]	довольно
+very	[ˈvɛri]	очень
+better	[ˈbɛtə]	лучше
+hard	[hɑːd]	трудно
+best	[bɛst]	лучший
+really	[ˈrɪəli]	действительно
+most	[məʊst]	наиболее
+still	[stɪl]	еще (все еще)
+yet (not yet)	[jɛt]	еще (еще нет)
+almost	[ˈɔːlməʊst]	почти
+easy	[ˈiːzi]	легко
+quite	[kwaɪt]	вполне (довольно)
+
+--     Вводные и соединительные слова (наречия)
+    maybe	[ˈmeɪbiː]	может быть
+actually	[ˈækʧʊəli]	на самом деле (вообще-то)
+probably	[ˈprɒbəbli]	вероятно
+of course	[əv] [kɔːs]	конечно
+perhaps	[pəˈhæps]	возможно
+possibly	[ˈpɒsəbli]	возможно
+unfortunately	[ʌnˈfɔːʧnɪtli]	к сожалению
+certainly	[ˈsɜːtnli]	конечно
+apparently	[əˈpærəntli]	по-видимому
+indeed	[ɪnˈdiːd]	действительно
+basically	[ˈbeɪsɪkəli]	в основном
+mainly	[ˈmeɪnli]	в основном
+especially	[ɪsˈpɛʃəli]	особенно
+particularly	[pəˈtɪkjʊləli]	особенно
+generally	[ˈʤɛnərəli]	в общем
+anyway	[ˈɛnɪweɪ]	в любом случае
+particularly	[pəˈtɪkjʊləli]	в особенности
+obviously	[ˈɒbvɪəsli]	очевидно
+moreover	[mɔːˈrəʊvə]	более того
+nevertheless	[ˌnɛvəðəˈlɛs]	тем не менее
+however	[haʊˈɛvə]	однако
+rather	[ˈrɑːðə]	скорее (скорее…, чем…)
+though	[ðəʊ]	хотя
+thus	[ðʌs]	Таким образом…
+so	[səʊ]	так
+otherwise	[ˈʌðəwaɪz]	в противном случае
---
+++ ZubrilkaEnglish(server)/src/main/resources/sql/3000words(part_5, prepositions and conjunctions).sql
@@ -0,0 +1,92 @@
+--     Пространство
+around (around the world)	[əˈraʊnd]	вокруг (вокруг света)
+at (at the gate)	[æt]	у (у ворот)
+by (house by the lake)	[baɪ]	около, у (дом у озера)
+in (in the room)	[ɪn]	в (в комнате)
+above (above the table)	[əˈbʌv]	над (над столом)
+among (the wolf among us)	[əˈmʌŋ]	среди (волк среди нас)
+behind (behind the building)	[bɪˈhaɪnd]	позади (позади здания)
+beside (beside the house)	[bɪˈsaɪd]	рядом с (рядом с домом)
+between (between two mountains)	[bɪˈtwiːn]	между (между двух гор)
+beyond (beyond the river)	[bɪˈjɒnd]	по ту сторону, за (за рекой)
+in front of (in front of TV)	[ɪn frʌntnɒv]	перед (перед телевизором)
+inside (inside the box)	[ɪnˈsaɪd]	внутри (внутри коробки)
+on (The cat is on the roof)	[ɒn]	на (кошка на крыше)
+out (to walk out)	[aʊt]	вне, наружу (выйти наружу)
+outside (Is there anybody outside?)	[ˌaʊtˈsaɪd]	снаружи (Есть кто-нибудь снаружи?)
+under (underground is under ground)	[ˈʌndə]	под (подземка под землей)
+within (within my heart)	[wɪˈðɪn]	внутри, в (в моем сердце)
+below (below sea level)	[bɪˈləʊ]	ниже (ниже уровня моря)
+
+--     Время
+    around (around nine o’clock)	[əˈraʊnd]	около (около 9 часов)
+at (at the midday)	[æt]	в (в полдень)
+before (before breakfast)	[bɪˈfɔː]	до (до завтрака)
+by (by two o’clock)	[baɪ]	к какому-то времени (к двум часам)
+in (I’ll be back in ten minutes)	[ɪn]	через какое-то время (вернусь через 10 минут)
+for (I’ve been waiting for you for an hour)	[fɔː]	на протяжении (я ждал тебя на протяжении часа)
+since (I’ve been studying English since I was 10)	[sɪns]	с какого-то времени (я учу английский с 10 лет)
+during (during the war)	[ˈdjʊərɪŋ]	во время (во время войны)
+within (He’s got to be here within an hour)	[wɪˈðɪn]	в течение (Он должен появиться здесь в течение часа)
+on (on Monday)	[ɒn]	в какой-то день недели (в понедельник)
+after (Don’t call me after 9PM)	[ˈɑːftə]	после (не звони мне после 9 вечера)
+till (from dusk till dawn)	[tɪl]	до (от рассвета до заката)
+
+--     Пространство
+    down (down the street)	[daʊn]	вниз (вниз по улице)
+up (up the street)	[ʌp]	вверх (вверх по улице)
+from (I am from Russia)	[frɒm]	из (я из России)
+across (go across the river)	[əˈkrɒs]	через (иди через реку)
+along (along the wall)	[əˈlɒŋ]	вдоль (вдоль стены)
+into (into the cave)	[ˈɪntuː]	в, внутрь (в пещеру)
+to (to the next stop)	[tuː]	к чему-то (к следующей остановке)
+toward (toward the city)	[təˈwɔːd]	по направлению к (по направлению к городу)
+through (through the forest)	[θruː]	через (через лес)
+over (over the fence)	[ˈəʊvə]	через (через забор)
+off (off here)	[ɒf]	из, откуда-то (отсюда)
+out of (out of the coverage area)	[aʊt ɒv]	наружу, вне (вне зоны покрытия)
+via (go via Moscow)	[ˈvaɪə]	через (ехать через Москву)
+past (go past the church)	[pɑːst]	мимо (пройти мимо церкви)
+
+--     Разные предлоги
+    with (coffe with sugar)	[wɪð]	с (кофе с сахаром)
+about (tell me about yourself)	[əˈbaʊt]	о (расскажите мне о себе)
+against (against war)	[əˈgɛnst]	против (против войны)
+without (without you)	[wɪˈðaʊt]	без (без тебя)
+including (two cars including one broken)	[ɪnˈkluːdɪŋ]	включая (две машины, включая одну сломанную)
+according to (according to the rules)	[əˈkɔːdɪŋ tʊ]	в соответствии с (в соответствии с правилами)
+because of (because of the car accident)	[bɪˈkəz ɒv]	из-за (из-за автомобильной аварии)
+despite (despite the bad weather)	[dɪsˈpaɪt]	несмотря на (несмотря на плохую погоду)
+instead of (new part instead of broken one)	[ɪnˈstɛd ɒv]	вместо (новая запчасть вместо сломанной)
+in spite of (in spite of the rain)	[ɪn spaɪt ɒv]	несмотря на (несмотря на дождь)
+regardless of (regardless of what you say)	[rɪˈgɑːdlɪs ɒv]	несмотря на, независимо от (несмотря на то, что ты скажешь)
+thanks to (thanks to the doctor, I’m feeling fine)	[θæŋks tʊ]	благодаря (благодаря доктору, я чувствую себя отлично)
+by means of (by means of tools)	[baɪ miːnz ɒv]	с помощью (с помощью инструментов)
+on behalf of (on behalf of my boss)	[ɒn bɪˈhɑːf ɒv]	от имени (от имени моего босса)
+for (this gift is for you)	[fɔː]	для (этот подарок для тебя)
+compared to (This is nothing compared to the real troubles)	[kəmˈpeə tʊ]	в сравнении с (это ничто в сравнении с настоящими трудностями)
+rather… than (they’d rather buy fish than meat)	[ˈrɑːðə]… [ðæn]	скорее, чем  (они купят скорее рыбу, чем мясо)
+rather than (they’d buy fish rather than meat)	[ˈrɑːðə ðæn]	скорее, чем\а не (они купят рыбу, а не мясо)
+
+--     Союзы
+    наверное вопросительные слова можно вынести в другой файл
+    And	[ænd]	и
+But	[bʌt]	но
+however	[haʊˈɛvə]	Однако
+As well as	[æz wɛl æz]	Также как
+Both… and	[bəʊθ]… [ænd]	И …, и …
+Either… or	[ˈaɪðə]… [ɔː] (UK) [ˈɪ: ðə] … [ɔː] (USA)	Или … или…,
+Neither… nor	[ˈnaɪðə]… [nɔː] (UK) [ˈnɪ: ðə] … [ɔː] (USA)	Ни …, ни …
+Before	[bɪˈfɔː]	До, перед
+After	[ˈɑːftə]	После
+While	[waɪl]	Во время
+Since	[sɪns]	С тех пор как
+Until	[ənˈtɪl]	До того как
+If	[ɪf]	Если
+Because	[bɪˈkɒz]	Потому что
+So	[səʊ]	Так что
+So (that)	[səʊ ðæt]	Чтобы, с целью
+Though, although	[ðəʊ] [ɔːlˈðəʊ]	Хотя
+Once	[wʌns]	Как только
+As soon as	[æz suːn æz]	Как только
+As long as	[æz lɒŋ æz]	Пока, при условии
