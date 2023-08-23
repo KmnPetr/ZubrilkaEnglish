283d07a05a1a4d26ae22ce9edba455e994a8ed3a
KmnPetr <kmn.petrichenko@yandex.ru>
2023-08-23 13:33:23 +0300

    слова прилагательные

---
+++ ZubrilkaEnglish(server)/src/main/resources/sql/3000words(part_3,adjectives).sql
@@ -0,0 +1,473 @@
+--     Время
+old	[əʊld]	старый
+young	[jʌŋ]	молодой
+early	[ˈɜːli]	ранний
+late	[leɪt]	поздний
+recent	[ˈriːsnt]	недавний
+current	[ˈkʌrənt]	текущий (нынешный)
+past	[pɑːst]	прошедший
+modern	[ˈmɒdən]	современный
+senior	[ˈsiːnjə]	старший
+future	[ˈfjuːʧə]	будущий
+previous	[ˈpriːvjəs]	предыдущий
+next	[nɛkst]	следующий
+annual	[ˈænjʊəl]	ежегодный
+following	[ˈfɒləʊɪŋ]	последующий
+daily	[ˈdeɪli]	ежедневный
+primary	[ˈpraɪməri]	первичный
+ancient	[ˈeɪnʃ(ə)nt]	древний
+initial	[ɪˈnɪʃəl]	начальный
+contemporary	[kənˈtɛmpərəri]	современный
+present	[ˈprɛznt]	настоящий (в наст. времени), присутствующий
+long-term	[ˈlɒŋtɜːm]	долгосрочный
+junior	[ˈʤuːnjə]	младший
+brief	[briːf]	краткий
+immediate	[ɪˈmiːdjət]	немедленный
+constant	[ˈkɒnstənt]	постоянный
+permanent	[ˈpɜːmənənt]	постоянный
+regular	[ˈrɛgjʊlə]	регулярный (обычный)
+ongoing	[ˈɒŋˌgəʊɪŋ]	продолжающийся
+sudden	[ˈsʌdn]	внезапный
+
+--     Человек
+    sure	[ʃʊə]	уверенный
+able	[ˈeɪbl]	способный (сделать что-то, быть кем-то)
+human	[ˈhjuːmən]	человеческий
+strong	[strɒŋ]	сильный
+free	[friː]	свободный
+certain	[ˈsɜːtn]	уверенный
+personal	[ˈpɜːsnl]	личный
+single	[ˈsɪŋgl]	одинокий (неженатый\незамужняя)
+private	[ˈpraɪvɪt]	личный (напр. личная жизнь)
+common	[ˈkɒmən]	обычный, распространенный, общий
+dead	[dɛd]	мертвый
+happy	[ˈhæpi]	счастливый
+serious	[ˈsɪərɪəs]	серьезный
+ready	[ˈrɛdi]	готовый
+simple	[ˈsɪmpl]	простой
+religious	[rɪˈlɪʤəs]	религиозный
+individual	[ˌɪndɪˈvɪdjʊəl]	индивидуальный
+tough	[tʌf]	крепкий, сильный
+positive	[ˈpɒzətɪv]	положительный
+successful	[səkˈsɛsfʊl]	успешный
+concerned	[kənˈsɜːnd]	обеспокоенный
+willing	[ˈwɪlɪŋ]	желающий (готовый к чему-то)
+interested	[ˈɪntrɪstɪd]	заинтересованный
+responsible	[rɪsˈpɒnsəbl]	ответственный
+afraid	[əˈfreɪd]	испуганный
+native	[ˈneɪtɪv]	родной (язык), коренной (житель)
+aware	[əˈweə]	знающий (осведомленный)
+active	[ˈæktɪv]	активный
+chief	[ʧiːf]	главный
+    cool	[kuːl]	крутой (клевый)
+direct	[dɪˈrɛkt]	прямой
+famous	[ˈfeɪməs]	известный
+familiar	[fəˈmɪljə]	знакомый
+male	[meɪl]	мужской
+alive	[əˈlaɪv]	живой
+strange	[streɪnʤ]	странный
+married	[ˈmærɪd]	женатый (замужняя)
+alone	[əˈləʊn]	одинокий
+sick	[sɪk]	больной
+smart	[smɑːt]	умный
+mean	[miːn]	подлый (злой)
+surprised	[səˈpraɪzd]	удивленный
+busy	[ˈbɪzi]	занятый
+careful	[ˈkeəfʊl]	осторожный
+tired	[ˈtaɪəd]	уставший
+proud	[praʊd]	гордый
+healthy	[ˈhɛlθi]	здоровый
+creative	[kri(ː)ˈeɪtɪv]	творческий
+lucky	[ˈlʌki]	везучий
+silent	[ˈsaɪlənt]	молчаливый
+leading	[ˈliːdɪŋ]	ведущий
+glad	[glæd]	довольный
+fat	[fæt]	полный (о фигуре)
+slim	[slɪm]	стройный (о фигуре)
+lean	[liːn]	стройный, тощий (о фигуре)
+nervous	[ˈnɜːvəs]	нервный
+pretty	[ˈprɪti]	симпатичная (о женщине)
+handsome	[ˈhænsəm]	симпатичный (о мужчине)
+sensitive	[ˈsɛnsɪtɪv]	чувствительный
+reasonable	[ˈriːznəbl]	разумный
+    aggressive	[əˈgrɛsɪv]	агрессивный
+mad	[mæd]	безумный
+pregnant	[ˈprɛgnənt]	беременная
+stupid	[ˈstjuːpɪd]	глупый
+blind	[blaɪnd]	слепой
+friendly	[ˈfrɛndli]	дружелюбный
+innocent	[ˈɪnəsənt]	невиновный
+attractive	[əˈtræktɪv]	привлекательный
+intellectual	[ˌɪntɪˈlɛktjʊəl]	интеллектуальный
+confident	[ˈkɒnfɪdənt]	уверенный
+pale	[peɪl]	бледный
+ill	[ɪl]	больной
+scared	[skeəd]	испуганный
+capable	[ˈkeɪpəbl]	способный
+fellow	[ˈfɛləʊ]	товарищеский
+advanced	[ədˈvɑːnst]	продвинутый (расширенный)
+favorite	[ˈfeɪvərɪt]	любимый
+wonderful	[ˈwʌndəfʊl]	чудесный
+emotional	[ɪˈməʊʃənl]	эмоциональный
+angry	[ˈæŋgri]	сердитый
+crazy	[ˈkreɪzi]	сумасшедший
+interesting	[ˈɪntrɪstɪŋ]	интересный
+fun	[fʌn]	веселый
+spiritual	[ˈspɪrɪtjʊəl]	духовный
+dramatic	[drəˈmætɪk]	драматичный
+sad	[sæd]	грустный
+amazing	[əˈmeɪzɪŋ]	удивительный
+psychological	[ˌsaɪkəˈlɒʤɪkəl]	психологический
+surprising	[səˈpraɪzɪŋ]	удивительный
+exciting	[ɪkˈsaɪtɪŋ]	увлекательный (захватывающий)
+
+--     Мораль
+    true	[truː]	правдивый
+wrong	[rɒŋ]	неверный, неправильный
+nice	[naɪs]	хороший, милый, приятный
+popular	[ˈpɒpjʊlə]	популярный
+dangerous	[ˈdeɪnʤrəs]	опасный
+moral	[ˈmɒrəl]	моральный
+negative	[ˈnɛgətɪv]	отрицательный (негативный)
+fair	[feə]	честный
+funny	[ˈfʌni]	смешной
+correct	[kəˈrɛkt]	правильный
+just	[ʤʌst]	справедливый
+terrible	[ˈtɛrəbl]	ужасный
+criminal	[ˈkrɪmɪnl]	преступный
+guilty	[ˈgɪlti]	виновный
+illegal	[ɪˈliːgəl]	незаконный
+violent	[ˈvaɪələnt]	жестокий (насильственный)
+holy	[ˈhəʊli]	святой
+false	[fɔːls]	ложный
+honest	[ˈɒnɪst]	честный
+
+--     Разнообразие
+    similar	[ˈsɪmɪlə]	похожий
+other	[ˈʌðə]	другой
+different	[ˈdɪfrənt]	другой (отличный от чего-то)
+various	[ˈveərɪəs]	различные
+general	[ˈʤɛnərəl]	общий (обычный)
+special	[ˈspɛʃəl]	особенный (специальный)
+specific	[spɪˈsɪfɪk]	конкретный
+only	[ˈəʊnli]	единственный
+final	[ˈfaɪnl]	окончательный
+average	[ˈævərɪʤ]	средний
+unique	[juːˈniːk]	уникальный
+equal	[ˈiːkwəl]	равный
+typical	[ˈtɪpɪk(ə)l]	типичный
+standard	[ˈstændəd]	стандартный
+rare	[reə]	редкий
+separate	[ˈsɛprɪt]	отдельный
+common	[ˈkɒmən]	общий
+multiple	[ˈmʌltɪpl]	многочисленные
+basic	[ˈbeɪsɪk]	базовый
+complete	[kəmˈpliːt]	полный (завершенный)
+diverse	[daɪˈvɜːs]	разнообразный
+classic	[ˈklæsɪk]	классический
+universal	[ˌjuːnɪˈvɜːsəl]	универсальный
+
+--     Логика
+    fine	[faɪn]	изящный (отличный)
+dark	[dɑːk]	темный
+beautiful	[ˈbjuːtəfʊl]	красивый
+light	[laɪt]	светлый, легкий
+bright	[braɪt]	яркий
+warm	[wɔːm]	теплый
+sweet	[swiːt]	сладкий
+visible	[ˈvɪzəbl]	видимый (заметный)
+hot	[hɒt]	острый, горячий
+ugly	[ˈʌgli]	отвратительный (о внешности)
+bitter	[ˈbɪtə]	горький
+supposed	[səˈpəʊzd]	предполагаемый, подразумеваемый
+appropriate	[əˈprəʊprɪɪt]	подходящий (уместный)
+relevant	[ˈrɛlɪvənt]	релевантный (имеющий отношение к чему-то)
+unknown	[ʌnˈnəʊn]	неизвестный
+secret	[ˈsiːkrɪt]	тайный
+consistent	[kənˈsɪstənt]	последовательный
+alternative	[ɔːlˈtɜːnətɪv]	альтернативный
+ultimate	[ˈʌltɪmɪt]	окончательный
+comprehensive	[ˌkɒmprɪˈhɛnsɪv]	всеобъемлющий
+
+--     Качество
+    качественный найти слово
+    new	[njuː]	новый
+good	[gʊd]	хороший
+great	[greɪt]	великий
+bad	[bæd]	плохой
+best	[bɛst]	лучший
+used	[juːzd]	использованный (подержанный)
+original	[əˈrɪʤənl]	оригинальный
+normal	[ˈnɔːməl]	нормальный
+useful	[ˈjuːsfʊl]	полезный
+proper	[ˈprɒpə]	тщательный (правильный)
+weak	[wiːk]	слабая
+excellent	[ˈɛksələnt]	отличный
+extreme	[ɪksˈtriːm]	экстремальный
+conventional	[kənˈvɛnʃənl]	обычный
+super	[ˈs(j)uːpə]	супер
+odd	[ɒd]	странный
+simple	[ˈsɪmpl]	простой
+intense	[ɪnˈtɛns]	интенсивный, плотный
+perfect	[ˈpɜːfɪkt]	идеальный
+ideal	[aɪˈdɪəl]	идеальный
+effective	[ɪˈfɛktɪv]	эффективный
+
+--     Размер
+    размер найти слово
+    high	[haɪ]	высокий
+big	[bɪg]	большой
+small	[smɔːl]	маленький
+large	[lɑːʤ]	большой
+long	[lɒŋ]	длинный (долгий)
+little	[ˈlɪtl]	маленький
+low	[ləʊ]	низкий
+short	[ʃɔːt]	короткий
+huge	[hjuːʤ]	огромный
+wide	[waɪd]	широкий
+tiny	[ˈtaɪni]	крошечный
+broad	[brɔːd]	широкий
+tall	[tɔːl]	высокий
+thin	[θɪn]	тонкий
+thick	[θɪk]	толстый
+increased	[ɪnˈkriːst]	увеличившийся
+enormous	[ɪˈnɔːməs]	громадный
+massive	[ˈmæsɪv]	массивный, массовый
+narrow	[ˈnærəʊ]	узкий
+straight	[streɪt]	прямой
+giant	[ˈʤaɪənt]	гигантский
+flat	[flæt]	плоский
+square	[skweə]	квадратный
+tremendous	[trɪˈmɛndəs]	огромный
+round	[raʊnd]	круглый
+prominent	[ˈprɒmɪnənt]	заметный (выступающий)
+
+--     Физические качества
+    clear	[klɪə]	ясный
+open	[ˈəʊpən]	открытый
+hot	[hɒt]	горячий
+cold	[kəʊld]	холодный
+heavy	[ˈhɛvi]	тяжелый
+tough	[tʌf]	жесткий
+powerful	[ˈpaʊəfʊl]	мощный
+quick	[kwɪk]	быстрый
+soft	[sɒft]	мягкий
+quiet	[ˈkwaɪət]	тихий
+cool	[kuːl]	прохладный
+dry	[draɪ]	сухой
+slow	[sləʊ]	медленный
+solid	[ˈsɒlɪd]	твердый
+sharp	[ʃɑːp]	острый (предмет, звук)
+live	[laɪv]	живой (в т.ч в значении «прямой эфир»)
+mass	[mæs]	массовый
+smooth	[smuːð]	гладкий
+wet	[wɛt]	мокрый
+fast	[fɑːst]	быстрый
+    rough	[rʌf]	грубый
+tight	[taɪt]	плотный
+raw	[rɔː]	сырой
+dirty	[ˈdɜːti]	грязный
+still	[stɪl]	замерший
+pure (water)	[pjʊə]	чистый (без примесей)
+steady	[ˈstɛdi]	устойчивый
+stable	[ˈsteɪbl]	стабильный
+vulnerable	[ˈvʌlnərəbl]	уязвимый
+loose	[luːs]	свободный (ненатянутый, напр. об одежде)
+accurate	[ˈækjʊrɪt]	точный
+whole	[həʊl]	весь (целиком)
+full	[fʊl]	полный
+physical	[ˈfɪzɪkəl]	физический
+entire	[ɪnˈtaɪə]	весь (целиком)
+clean	[kliːn]	чистый
+empty	[ˈɛmpti]	пустой
+golden	[ˈgəʊldən]	золотой
+wooden	[ˈwʊdn]	деревянный
+broken	[ˈbrəʊkən]	сломанный
+visual	[ˈvɪzjʊəl]	визуальный
+
+--     Экономика
+    economic	[ˌiːkəˈnɒmɪk]	экономический
+free	[friː]	бесплатный
+financial	[faɪˈnænʃəl]	финансовый
+commercial	[kəˈmɜːʃəl]	коммерческий
+rich	[rɪʧ]	богатый
+expensive	[ɪksˈpɛnsɪv]	дорогой
+cheap	[ʧiːp]	дешевый
+competitive	[kəmˈpɛtɪtɪv]	конкурентоспособный
+valuable	[ˈvæljʊəbl]	ценный
+poor	[pʊə]	бедный
+remaining	[rɪˈmeɪnɪŋ]	оставшийся
+double	[ˈdʌbl]	двойной
+numerous	[ˈnjuːmərəs]	многочисленные
+increasing	[ɪnˈkriːsɪŋ]	увеличивающийся
+total (total number)	[ˈtəʊtl]	общий (напр.общее количество)
+single	[ˈsɪŋgl]	одиночный
+
+--     Важность, вероятность
+    main	[meɪn]	главный
+major	[ˈmeɪʤə]	основной, важный, крупный
+significant	[sɪgˈnɪfɪkənt]	значительный
+particular	[pəˈtɪkjʊlə]	специфический (особенный)
+critical	[ˈkrɪtɪkəl]	критический, решающий
+essential	[ɪˈsɛnʃəl]	важный (существенный)
+key	[kiː]	ключевой
+crucial	[ˈkruːʃəl]	решающий (значимый)
+extraordinary	[ɪksˈtrɔːdnri]	необычайный
+vital	[ˈvaɪtl]	жизненный (жизненно важный)
+substantial	[səbˈstænʃəl]	существенный
+minor	[ˈmaɪnə]	незначительный, младший
+considerable	[kənˈsɪdərəbl]	значительный
+extra	[ˈɛkstrə]	дополнительный
+actual	[ˈækʧʊəl]	актуальный (настоящий)
+practical	[ˈpræktɪkəl]	практический
+fundamental	[ˌfʌndəˈmɛntl]	фундаментальный
+real	[rɪəl]	реальный
+possible	[ˈpɒsəbl]	возможный
+likely	[ˈlaɪkli]	вероятный
+necessary	[ˈnɛsɪsəri]	необходимый
+opposite	[ˈɒpəzɪt]	противоположный
+supposed	[səˈpəʊzd]	предполагаемый
+potential	[pəʊˈtɛnʃəl]	потенциальный
+appropriate	[əˈprəʊprɪɪt]	подходящий (уместный)
+unlikely	[ʌnˈlaɪkli]	маловероятный
+important	[ɪmˈpɔːtənt]	важный
+
+--     Политика
+    national	[ˈnæʃənl]	национальный
+political	[pəˈlɪtɪkəl]	политический
+social	[ˈsəʊʃəl]	социальный, общественный
+public	[ˈpʌblɪk]	общественный, государственный
+military	[ˈmɪlɪtəri]	военный
+federal	[ˈfɛdərəl]	федеральный
+international	[ˌɪntə(ː)ˈnæʃənl]	международный
+foreign	[ˈfɒrɪn]	иностранный
+democratic	[ˌdɛməˈkrætɪk]	демократический
+legal	[ˈliːgəl]	законный (правовой)
+traditional	[trəˈdɪʃənl]	традиционный
+cultural	[ˈkʌlʧərəl]	культурный
+civil	[ˈsɪvl]	гражданский
+safe	[seɪf]	безопасный
+nuclear	[ˈnjuːklɪə]	ядерный
+independent	[ˌɪndɪˈpɛndənt]	независимый
+united	[jʊˈnaɪtɪd]	объединенный
+    domestic	[dəʊˈmɛstɪk]	домашний (внутренний — в политич.смысле)
+corporate	[ˈkɔːpərɪt]	корпоративный
+urban	[ˈɜːbən]	городской
+regional	[ˈriːʤənl]	региональный
+ethnic	[ˈɛθnɪk]	этнический
+conservative	[kənˈsɜːvətɪv]	консервативный
+industrial	[ɪnˈdʌstrɪəl]	промышленный
+historical	[hɪsˈtɒrɪkəl]	исторический
+presidential	[ˌprɛzɪˈdɛnʃəl]	президентский
+liberal	[ˈlɪbərəl]	либеральный
+rural	[ˈrʊərəl]	сельский
+historic	[hɪsˈtɒrɪk]	исторический
+armed	[ɑːmd]	вооруженный
+constitutional	[ˌkɒnstɪˈtjuːʃənl]	конституционный
+strategic	[strəˈtiːʤɪk]	стратегический
+defensive	[dɪˈfɛnsɪv]	оборонительный
+joint	[ʤɔɪnt]	совместный
+mutual	[ˈmjuːtjʊəl]	взаимный
+official	[əˈfɪʃəl]	официальный
+
+--     Некоторые свойства
+        hard	[hɑːd]	трудный
+easy	[ˈiːzi]	легкий
+difficult	[ˈdɪfɪkəlt]	трудный
+complex	[ˈkɒmplɛks]	сложный (комплексный)
+impossible	[ɪmˈpɒsəbl]	невозможный
+obvious	[ˈɒbvɪəs]	очевидный
+comfortable	[ˈkʌmfətəbl]	удобный, комфортный
+convenient	[kənˈviːnjənt]	удобный
+handy	[ˈhændi]	удобный, сподручный
+complicated	[ˈkɒmplɪkeɪtɪd]	сложный
+severe (pain)	[sɪˈvɪə]	жестокий (напр. условия, боль, погода)
+apparent	[əˈpærənt]	очевидный
+elementary	[ˌɛlɪˈmɛntəri]	элементарный
+regular	[ˈrɛgjʊlə]	регулярный (обычный)
+extra	[ˈɛkstrə]	дополнительный
+unusual	[ʌnˈjuːʒʊəl]	необычный
+formal	[ˈfɔːməl]	формальный
+ordinary	[ˈɔːdnri]	обычный
+usual	[ˈjuːʒʊəl]	обычный
+so-called	[ˈsəʊˈkɔːld]	так называемый
+assistant	[əˈsɪstənt]	вспомогательный
+additional	[əˈdɪʃənl]	дополнительный
+remarkable	[rɪˈmɑːkəbl]	примечательный
+        medical	[ˈmɛdɪkəl]	медицинский
+professional	[prəˈfɛʃənl]	профессиональный
+academic	[ˌækəˈdɛmɪk]	академический
+educational	[ˌɛdju(ː)ˈkeɪʃənl]	образовательный
+scientific	[ˌsaɪənˈtɪfɪk]	научный
+online	[ˈɒnˌlaɪn]	онлайн
+digital	[ˈdɪʤɪtl]	цифровой
+electronic	[ɪlɛkˈtrɒnɪk]	электронный
+electric	[ɪˈlɛktrɪk]	электрический
+genetic	[ʤɪˈnɛtɪk]	генетический
+chemical	[ˈkɛmɪkəl]	химический
+working	[ˈwɜːkɪŋ]	работающий
+technical	[ˈtɛknɪkəl]	технический
+musical	[ˈmjuːzɪkəl]	музыкальный
+literary	[ˈlɪtərəri]	литературный
+clinical	[ˈklɪnɪkəl]	клинический
+developing	[dɪˈvɛləpɪŋ]	находящийся в разработке
+
+--         Пространство
+        top	[tɒp]	верхний (главный)
+right	[raɪt]	правильный (правый)
+local	[ˈləʊkəl]	местный
+available	[əˈveɪləbl]	доступный
+central	[ˈsɛntrəl]	центральный
+left	[lɛft]	левый (оставшийся)
+close	[kləʊs]	близкий (закрытый)
+far	[fɑː]	далекий
+deep	[diːp]	глубокий
+middle	[ˈmɪdl]	средний
+western	[ˈwɛstən]	западный
+southern	[ˈsʌðən]	южный
+front	[frʌnt]	передний
+eastern	[ˈiːstən]	восточный
+northern	[ˈnɔːðən]	северный
+internal	[ɪnˈtɜːnl]	внутренний
+upper	[ˈʌpə]	верхний
+vast	[vɑːst]	широкий (обширный)
+limited	[ˈlɪmɪtɪd]	ограниченный
+outside	[ˌaʊtˈsaɪd]	внешний
+nearby	[ˈnɪəbaɪ]	находящийся поблизости
+inner	[ˈɪnə]	внутренний
+biological	[ˌbaɪəʊˈlɒʤɪkəl]	биологический
+distant	[ˈdɪstənt]	удаленный
+bottom	[ˈbɒtəm]	нижний
+remote	[rɪˈməʊt]	удаленный (дистанционный)
+external	[ɛksˈtɜːnl]	внешний
+lost	[lɒst]	потерянный
+extensive	[ɪksˈtɛnsɪv]	обширный
+natural	[ˈnæʧrəl]	природный (естественный)
+environmental	[ɪnˌvaɪərənˈmɛntl]	относящийся к окружающей среде
+fresh	[frɛʃ]	свежий
+global	[ˈgləʊbəl]	глобальный
+wild	[waɪld]	дикий
+growing	[ˈgrəʊɪŋ]	растущий
+living	[ˈlɪvɪŋ]	живущий
+racial	[ˈreɪʃəl]	расовый
+solar	[ˈsəʊlə]	солнечный (напр. Солнечная система)
+existing	[ɪgˈzɪstɪŋ]	существующий
+
+--         Цвета
+        color (colour)	[ˈkʌlə]	цвет
+black	[blæk]	черный
+white	[waɪt]	белый
+dark	[dɑːk]	темный
+light	[laɪt]	светлый
+red	[rɛd]	красный
+orange	[ˈɒrɪnʤ]	оранжевый
+yellow	[ˈjɛləʊ]	желтый
+green	[griːn]	зеленый
+blue	[bluː]	голубой, синий
+purple	[ˈpɜːpl]	фиолетовый (темно-фиолетовый)
+violet	[ˈvaɪəlɪt]	фиолетовый (фиалковый)
+pink	[pɪŋk]	розовый
+silver	[ˈsɪlvə]	серебристый
+gold, golden	[gəʊld] [gəʊldən]	золотой
+brown	[braʊn]	коричневый
+grey (gray)	[greɪ]	серый
