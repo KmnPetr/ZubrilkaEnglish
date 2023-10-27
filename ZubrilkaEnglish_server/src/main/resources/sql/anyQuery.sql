9179601dcb7e46596df29a5de110a3a8475b2bca
KmnPetr <kmn.petrichenko@yandex.ru>
2023-10-27 23:09:56 +0300

    commit

--- ZubrilkaEnglish_server/src/main/resources/sql/anyQuery.sql
+++ ZubrilkaEnglish_server/src/main/resources/sql/anyQuery.sql
@@ -2,3 +2,10 @@ UPDATE word SET groupwrd='FIRST300WORDS'WHERE id IN (1,2,3)
 
 
 UPDATE person SET role='ROLE_ADMIN' WHERE id=1;
+
+SELECT*FROM word
+WHERE foreign_word IN (
+    SELECT foreign_word
+    FROM word
+    GROUP BY foreign_word
+    HAVING COUNT(*) > 1);
