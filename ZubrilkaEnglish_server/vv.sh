58cb4d5031491afc20512a0e3053b2a3cec3664e
root <root@608686.cloud4box.ru>
2023-12-05 20:09:55 +0300

    working on a script to change the version

---
+++ ZubrilkaEnglish_server/vv.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Укажите путь к вашему XML файлу
+file="pom.xml"
+apt-get install -y xmlstarlet
+
+VERSION=$(xmlstarlet sel -N p=http://maven.apache.org/POM/4.0.0 -t -v "/p:project/p:version" pom.xml)
+
+echo "    "
+echo ${VERSION}
+echo "    "
+echo "    "
+IFS='.' read -r -a arr <<< "$VERSION"
+((arr[2]++))
+result=$(IFS="."; echo "${arr[*]}")
+
+echo "NewVersion"
+echo $result
+echo "    "
+echo " "
+
+xmlstarlet ed -L -N p=http://maven.apache.org/POM/4.0.0 -u "/p:project/p:version" -v $result pom.xml
+
+apt-get remove xmlstarlet -y
