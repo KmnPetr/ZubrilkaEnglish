f3dce7fdba0b188120955dc45754c423b4a42b20
KmnPetr <kmn.petrichenko@yandex.ru>
2023-12-07 22:54:15 +0300

    commit

--- ZubrilkaEnglish_server/src/main/java/com/example/ZubrilkaEnglishServer/controllers/HealthCheckController.java
+++ ZubrilkaEnglish_server/src/main/java/com/example/ZubrilkaEnglishServer/controllers/HealthCheckController.java
@@ -1,6 +1,5 @@
 package com.example.ZubrilkaEnglishServer.controllers;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,11 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/healthcheck")
 public class HealthCheckController {
 
-    @Value("${checkenv}")
-    private String checkenv;
-
     @GetMapping()
     public ResponseEntity<String> healthCheck(){
-        return new ResponseEntity<>("All ok."+checkenv, HttpStatus.OK);
+        return new ResponseEntity<>("All ok.", HttpStatus.OK);
     }
 }
