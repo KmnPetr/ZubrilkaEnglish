0a60bf4accdaa57a8c50c2414e28d8dfc8d00fe9
KmnPetr <kmn.petrichenko@yandex.ru>
2023-12-09 12:51:15 +0300

    change healthcheck

--- ZubrilkaEnglish_server/src/main/java/com/example/ZubrilkaEnglishServer/controllers/HealthCheckController.java
+++ ZubrilkaEnglish_server/src/main/java/com/example/ZubrilkaEnglishServer/controllers/HealthCheckController.java
@@ -1,5 +1,6 @@
 package com.example.ZubrilkaEnglishServer.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,9 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/healthcheck")
 public class HealthCheckController {
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.password}")
+    private String password;
 
     @GetMapping()
     public ResponseEntity<String> healthCheck(){
-        return new ResponseEntity<>("All ok.", HttpStatus.OK);
+
+        return new ResponseEntity<>(
+                "All ok.   URL = "+url+"   password="+password,
+                HttpStatus.OK
+        );
     }
 }
