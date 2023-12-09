8f7fabd0ee4a3d0d405d1e92f401f8151a9e705c
KmnPetr <kmn.petrichenko@yandex.ru>
2023-12-09 16:04:03 +0300

    commit

--- ZubrilkaEnglish_server/src/main/java/com/example/ZubrilkaEnglishServer/controllers/HealthCheckController.java
+++ ZubrilkaEnglish_server/src/main/java/com/example/ZubrilkaEnglishServer/controllers/HealthCheckController.java
@@ -10,16 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/healthcheck")
 public class HealthCheckController {
-    @Value("${spring.datasource.url}")
-    private String url;
-    @Value("${spring.datasource.password}")
-    private String password;
 
     @GetMapping()
     public ResponseEntity<String> healthCheck(){
 
         return new ResponseEntity<>(
-                "All ok.   URL = "+url+"   password="+password,
+                "All ok.",
                 HttpStatus.OK
         );
     }
