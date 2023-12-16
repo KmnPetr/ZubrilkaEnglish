3968c3d6cbbc0a4e8c1ab7b22181ecef29d2f462
KmnPetr <kmn.petrichenko@yandex.ru>
2023-12-16 21:44:44 +0300

    change context path

--- ZubrilkaEnglish_server/src/main/java/com/example/ZubrilkaEnglishServer/ZubrilkaEnglishServerApplication.java
+++ ZubrilkaEnglish_server/src/main/java/com/example/ZubrilkaEnglishServer/ZubrilkaEnglishServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class ZubrilkaEnglishServerApplication {
