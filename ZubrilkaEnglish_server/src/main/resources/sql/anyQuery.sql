322487638edcbacf14c8a8329712e2650ca47d49
KmnPetr <kmn.petrichenko@yandex.ru>
2023-12-29 20:49:53 +0300

    replace sql files

--- ZubrilkaEnglish_server/src/main/resources/sql/anyQuery.sql
+++
@@ -1,9 +0,0 @@
-
-UPDATE person SET role='ROLE_ADMIN' WHERE id=1;
-
-SELECT*FROM word
-WHERE foreign_word IN (
-    SELECT foreign_word
-    FROM word
-    GROUP BY foreign_word
-    HAVING COUNT(*) > 1);
