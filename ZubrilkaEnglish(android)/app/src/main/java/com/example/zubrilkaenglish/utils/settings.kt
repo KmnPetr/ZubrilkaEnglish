4eeec2c066462717a8a7ea665e72821618118cf1
KmnPetr <kmn.petrichenko@yandex.ru>
2024-01-04 06:06:34 -0700

    commit

--- ZubrilkaEnglish(android)/app/src/main/java/com/example/zubrilkaenglish/utils/settings.kt
+++ ZubrilkaEnglish(android)/app/src/main/java/com/example/zubrilkaenglish/utils/settings.kt
@@ -1,6 +1,6 @@
 package com.example.zubrilkaenglish.utils
 
-val URL:String="https://5982246.cloud4box.ru"
+const val URL:String="https://598224.cloud4box.ru"
 
 //используемый формат для записи времени засыпания и пробуждения карточек
 val SIM_FORM_DATE: String = "dd.MM.yyyy"
