11178862001488c229b43384431c1b826693f8d0
KmnPetr <kmn.petrichenko@yandex.ru>
2024-01-05 11:01:02 -0700

    commit

---
+++ ze-app/src/main/java/com/example/zeapp/ZeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.zeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeAppApplication.class, args);
+	}
+
+}
