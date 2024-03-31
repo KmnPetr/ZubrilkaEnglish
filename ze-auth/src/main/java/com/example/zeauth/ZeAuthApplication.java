3fa1e83476e41fe896777ab755154071b90643e6
KmnPetr <kmn.petrichenko@yandex.ru>
2024-03-31 18:12:26 +0300

    some changes

---
+++ ze-auth/src/main/java/com/example/zeauth/ZeAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.zeauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeAuthApplication.class, args);
+	}
+
+}
---
+++ ze-auth/src/test/java/com/example/zeauth/ZeAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zeauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
