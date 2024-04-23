1b84d73c1f4d4c01eae05de88f46ab40607b1aa5
KmnPetr <kmn.petrichenko@yandex.ru>
2024-04-23 21:27:05 +0300

    new voices

--- WordsManager/src/main/java/com/example/WordsManager/services/PropService.java
+++ WordsManager/src/main/java/com/example/WordsManager/services/PropService.java
@@ -5,7 +5,6 @@ import com.example.WordsManager.repositories.PropRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import reactor.core.Disposable;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
@@ -19,13 +18,6 @@ public class PropService {
     }
 
 
-    /**
-     * метод установит новое значение dictionary_version в таблице properties
-     */
-    @Transactional
-    public void setDictionaryVersion() {
-//        propRepository.setNewValue("dictionary_version", ZonedDateTime.now().toString()); //TODO теперь используем int вместо формата времени
-    }
 
     /**
      * метод выдаст версию последнего обновления словаря Word БД
@@ -40,24 +32,6 @@ public class PropService {
 //    @Transactional(isolation = Isolation.REPEATABLE_READ)
     @Transactional
     public Mono<Integer> increaseDictionaryVersion() {
-//        propRepository.findByKey("dictionary_version").subscribe(
-//                next -> {
-//                    System.out.println("Обработка корректного ответа");
-//                    int version = Integer.parseInt(next.getValue());
-//                    version++;
-//                    Mono<Integer> dictionaryVersion = propRepository.setNewValue("dictionary_version", String.valueOf(version));
-//                    dictionaryVersion.subscribe(System.out::println);
-//
-//                },
-//                error -> {
-//                    System.out.println("Обработка ошибки");
-//                    error.printStackTrace();
-//                    },
-//                () -> {
-//                    System.out.println("Обработка завершения потока");
-//                }
-//
-//        );
 
         return propRepository.findByKey("dictionary_version")
                 .publishOn(Schedulers.boundedElastic())
