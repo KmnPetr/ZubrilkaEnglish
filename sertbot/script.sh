96d43c5f92b700baa8464eefea2a47b48b7301ac
KmnPetr <kmn.petrichenko@yandex.ru>
2024-05-27 00:36:19 +0300

    setup sertbot

--- sertbot/script.sh
+++ sertbot/script.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
 # Получить и установить сертификат
-certbot certonly --standalone --preferred-challenges http --agree-tos --email kmn.petrichenko@yandex.ru -d 608686.cloud4box.ru --non-interactive
+certbot certonly --email kmn.petrichenko@yandex.ru -d 608686.cloud4box.ru --non-interactive
 
 # Добавить задание в cron для еженедельного обновления сертификата
-echo "0 0 * * 0 certbot renew --standalone --preferred-challenges http --agree-tos --email kmn.petrichenko@yandex.ru -d 608686.cloud4box.ru --non-interactive" | crontab -
+echo "0 0 * * 0 certbot renew --email kmn.petrichenko@yandex.ru -d 608686.cloud4box.ru --non-interactive" | crontab -
 
 # Запустить crond в фоновом режиме
 crond -f
+
+
+
+
+## Получить и установить сертификат
+#certbot certonly --standalone --preferred-challenges http --agree-tos --email kmn.petrichenko@yandex.ru -d 608686.cloud4box.ru --non-interactive
+#
+## Добавить задание в cron для еженедельного обновления сертификата
+#echo "0 0 * * 0 certbot renew --standalone --preferred-challenges http --agree-tos --email kmn.petrichenko@yandex.ru -d 608686.cloud4box.ru --non-interactive" | crontab -
+#
+## Запустить crond в фоновом режиме
+#crond -f
