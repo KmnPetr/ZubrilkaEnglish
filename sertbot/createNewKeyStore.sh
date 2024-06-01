a858d8ac46b7d6c7e0bd0b9589be2365ecaf4e22
KmnPetr <kmn.petrichenko@yandex.ru>
2024-06-01 19:54:56 +0300

    commit

---
+++ sertbot/createNewKeyStore.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#создаем файл с ключами который может прочитать сервер java
+rm /etc/letsencrypt/live/$DOMAIN_NAME/keystore.p12 || true
+openssl pkcs12 -export -in /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem -inkey /etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem -out /etc/letsencrypt/live/$DOMAIN_NAME/keystore.p12 -name tomcat -CAfile /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem -caname root -passout pass:password
+echo "new file key store was created"
--- sertbot/renew.sh
+++ sertbot/renew.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
+#скрипт вызывается хроном переодически обновляя сертификат ssl если это необходимо
+# а также создавая новый файл с ключами keystore.p12 который может прочитать сервер java
+
 
 echo "renew.sh started!!!"
 
-certbot renew --email $MY_EMAIL -d $DOMAIN_NAME --non-interactive
-
-rm /etc/letsencrypt/live/$DOMAIN_NAME/keystore.p12 || true
-openssl pkcs12 -export -in /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem -inkey /etc/letsencrypt/live/$DOMAIN_NAME/privkey.pem -out /etc/letsencrypt/live/$DOMAIN_NAME/keystore.p12 -name tomcat -CAfile /etc/letsencrypt/live/$DOMAIN_NAME/fullchain.pem -caname root -passout pass:password
+certbot renew --non-interactive --deploy-hook "/usr/local/bin/createNewKeyStore.sh"
