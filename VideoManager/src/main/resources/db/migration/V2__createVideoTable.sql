c07cf51853c266a00c959777a16061c04898cfe1
KmnPetr <kmn.petrichenko@yandex.ru>
2024-12-21 12:53:13 +0300

    saving new word and voice objects

--- VideoManager/src/main/resources/db/migration/V2__createVideoTable.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE video (
-    uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    file_name VARCHAR NOT NULL,
-    bytes BYTEA NOT NULL
-);
--- VideoManager/src/main/resources/db/migration/V4__createTranslationTable.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE translation (
-    uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    version BIGINT DEFAULT 0,
-    phrases TEXT
-);
